f391f0d8ef5f01502df1e82ded08469fe3c50da7
Agustin Schipani <agus-calderon-schip03@outlook.com>
2022-11-10 17:32:39 -0300

    Pulido Final
    ..

--- StellarRaiders/Juego/UI/PantallaFinalJuego.gd
+++ StellarRaiders/Juego/UI/PantallaFinalJuego.gd
@@ -1,3 +1,4 @@
+class_name PantallaFinalJuego
 extends Node
 
 
--- StellarRaiders/Juego/UI/PantallaHistoria.gd
+++ StellarRaiders/Juego/UI/PantallaHistoria.gd
@@ -1,12 +1,12 @@
-class_name MenuPrincipal
+class_name PantallaHistoria
 extends Node
+
 
 export(String, FILE, "*.tscn") var nivel_siguiente = ""
 onready var musica: Node = $MusicaStellarRaiders
 
 
 func _ready() -> void:
-	OS.set_window_fullscreen(true)
 	musica.ejecutar_musica(musica.get_lista_musica().menu_principal)
 
 
@@ -14,8 +14,3 @@ func _on_Button_pressed() -> void:
 	musica.ejecutar_sfx_botones()
 # warning-ignore:return_value_discarded
 	get_tree().change_scene(nivel_siguiente)
-
-
-func _on_ButtonSalir_pressed() -> void:
-	musica.ejecutar_sfx_botones()
-	get_tree().quit()
